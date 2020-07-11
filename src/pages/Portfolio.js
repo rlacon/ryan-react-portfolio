21d5429b11ab25dce66bba669f6019ba8568811e
Ryan Lacon <ryan.lacon@gmail.com>
2020-07-11 17:30:40 -0400

    Formatted card desceriptions better

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -19,10 +19,32 @@ const transitionStyles = {
 
 function Portfolio(props) {
   const [data] = useState([
-    { image: "/images/ghost_writer.png", title: "Ghost Writer", description: "A collaborative writing app that allows users to write different sections of a story. Once all sections have been written, the story moves to a Completed Stories page. Create an account, or log in with Username: guest and Password: guest123 to try out the app.", category: "Development", mainLink: "https://ghost-writer.herokuapp.com/", githubLink: "https://github.com/rlacon/Project-3---Ghost-writer-social/" },
-    { image: "/images/google-book-search-pic.png", title: "Google Books Search", description: "Demonstration of the Google Books API to input search queries and return a list of matching results.", category: "Development", mainLink: "https://rlacon-book-search.herokuapp.com/", githubLink: "https://github.com/rlacon/GoogleBooksSearch/" },
-    { image: "/images/game-matching-app-pic.png", title: "Game Matcher", description: "An app that allows users to select a criteria of favorite games and matches with other users who have made selections. Create an account, or log in with Username: 123 and Password: 123 to try out the app.", category: "Development", mainLink: "https://www.google.com/", githubLink: "https://www.google.com/" },
-  ]);
+    {
+      image: "/images/ghost_writer.png",
+      title: "Ghost Writer",
+      description: "A collaborative writing app that allows users to write different sections of a story. Once all sections have been written, the story moves to a Completed Stories page. Create an account, or log in with the following credentials:",
+      userName: 'Username: guest',
+      pass: 'Password: guest123',
+      category: "Development",
+      mainLink: "https://ghost-writer.herokuapp.com/",
+      githubLink: "https://github.com/rlacon/Project-3---Ghost-writer-social/"
+    }, {
+      image: "/images/google-book-search-pic.png",
+      title: "Google Books Search",
+      description: "Demonstration of the Google Books API to input search queries and return a list of matching results.",
+      category: "Development",
+      mainLink: "https://rlacon-book-search.herokuapp.com/",
+      githubLink: "https://github.com/rlacon/GoogleBooksSearch/"
+    }, {
+      image: "/images/game-matching-app-pic.png",
+      title: "Game Matcher",
+      description: "An app that allows users to select a criteria of favorite games and matches with other users who have made selections. Create an account, or log in with the following credentials:",
+      userName: 'Username: 123',
+      pass: 'Password: 123',
+      category: "Development",
+      mainLink: "https://www.google.com/",
+      githubLink: "https://www.google.com/"
+    }]);
 
   const [fade, setFade] = useState(false);
 
