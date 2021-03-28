66da4ba514016f78b410d7e06cd8dc0a2b819060
Ryan Lacon <ryan.lacon@gmail.com>
2021-03-28 11:08:26 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
