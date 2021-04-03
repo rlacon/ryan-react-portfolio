353f8475a4736ccaecc2fec0beee706683f93b4e
Ryan Lacon <ryan.lacon@gmail.com>
2021-04-03 11:23:53 -0400

    Styling changes

--- src/pages/UDisc.js
+++ src/pages/UDisc.js
@@ -6,11 +6,11 @@ class UDisc extends Component {
     return (
       <div>
         <div className="container mx-auto px-6 py-4">
-          <h1 className="font-bold text-5xl text-center">U-Disc App Redesign</h1>
+          <h1 className="font-bold text-5xl text-center text-purple-500">U-Disc App Redesign</h1>
           <br />
           <p className="text-xl">I'm an avid user of the U-Disc app. I had some ideas to build out a more robust social component:</p>
           <br />
-          <ul className="text-xl">
+          <ul className="text-gray-700 text-lg leading-8">
             <li>1. There will be a dedicated friends list.</li>
             <li>2. It would now be possible to add friends through a QR code.</li>
             <li>3. The app will display where friends are currently playing when they begin a new score card.</li>
