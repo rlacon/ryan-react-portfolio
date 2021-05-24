0f2ca3eb9ba99a4b586c8967bc0301fa675beede
Ryan Lacon <ryan.lacon@gmail.com>
2021-05-24 13:25:46 -0400

    Change ordering of rotated text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ function Hero(props) {
     <div className="py-20 bg-gradient-t-blue-burst">
       <div className="container mx-auto px-6">
         <RotatingTextStyle>
-          <h2 className="py-8 text-5xl font-bold mb-2 text-white">I'm a <ReactRotatingText className="RotatingTextStyle" items={['Developer', 'Designer', 'Researcher', 'Problem-Solver', 'Googling Expert']} /></h2>
+          <h2 className="py-8 text-5xl font-bold mb-2 text-white">I'm a <ReactRotatingText className="RotatingTextStyle" items={['Designer', 'Developer', 'Researcher', 'Problem-Solver', 'Googling Expert']} /></h2>
         </RotatingTextStyle>
         {/* <AnchorLink offset='100' href="#skills" className="underline text-white font-bold py-4 pr-8 uppercase tracking-wider">Skills</AnchorLink>
         <NavLink to="/portfolio" className="underline text-white font-bold py-4 px-8 uppercase tracking-wider" href="#work">See my work</NavLink>
